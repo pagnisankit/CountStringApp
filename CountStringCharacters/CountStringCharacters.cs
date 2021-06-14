2d7ebc1f16f668bad4f48a2c59bc7edb5d42e7e7
ankitPagnis <ankit_pagnis@yahoo.com>
2021-06-14 17:27:21 +0530

    initial commit for app

---
+++ CountStringCharacters/CountStringCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountStringCharacters
+{
+    class CountStringCharacters
+    {
+        public void method()
+        {
+            string str = "";
+            Console.Write("Provide a string");
+            str = Console.ReadLine();
+            Dictionary<string, int> strdic = new Dictionary<string, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                string key = str[i].ToString();
+                char keychar = str[i];
+                int val = str.Count(x=>(x==keychar));
+                if (!strdic.ContainsKey(key))
+                {
+                    strdic.Add(key, val);
+                }
+            }
+
+            foreach (var items in strdic)
+            {
+                Console.WriteLine(items.Key + " - " + items.Value);
+            }
+        }
+    }
+}
---
+++ CountStringCharacters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountStringCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CountStringCharacters str = new CountStringCharacters();
+                str.method();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            
+        }
+    }
+}
